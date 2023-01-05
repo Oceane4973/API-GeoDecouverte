e3118c63585c776ae6be2ace21d59f18600e218f
Océane Monges <oceane.monges4973@gmail.com>
2023-01-05 17:11:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-/*const express = require('express')
-//const dbManagerObject = require('./dbManager.js').dbManager
+const express = require('express')
+const dbManagerObject = require('./dbManager.js').dbManager
 
 const API = express()
 
@@ -13,29 +13,26 @@ API.use((req, res, next) => {
 })
 
 API.get('/images', (req, res, next)=>{
-    //res.json({images : dbManagerObject.getAllImages()})
-    res.json("hey")
-    //res.status(200).json("Erreur de connexion")
+    res.json({images : dbManagerObject.getAllImages()})
+    res.status(200).json("Erreur de connexion")
 })
 
-
-/*
-API.post('/images/add', (req, res)=>{
+API.post('/images/add', (req, res, next)=>{
     res.json( dbManagerObject.addImage(req.body.image))
-    //res.status(200).json("Erreur de connexion")
+    res.status(200).json("Erreur de connexion")
 })
 
-API.get('/images/city_filter', (req, res)=>{
+API.get('/images/city_filter', (req, res, next)=>{
     res.json( dbManagerObject.getImageWithNameCity(req.body.city))
-    //res.status(200).json("Erreur de connexion")
+    res.status(200).json("Erreur de connexion")
 })
 
-API.get('/images/country_filter', (req, res)=>{
+API.get('/images/country_filter', (req, res, next)=>{
     res.json( dbManagerObject.getImageWithNameCountry(req.body.country))
-    //res.status(200).json("Erreur de connexion")
-})*/
+    res.status(200).json("Erreur de connexion")
+})
 
-/*
+
 API.listen(5000, ()=>{
     console.log("API démarrée")
 
@@ -59,29 +56,6 @@ API.listen(5000, ()=>{
     console.log(dbManagerObject.getImageWithNameCountry("France"))
     console.log(dbManagerObject.getImageWithNameCountry("USA"))
     console.log(dbManagerObject.getImageWithNameCountry(""))*/
-//})
-
-const express = require('express')
-
-const API = express()
-
-API.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Credentials', 'true')
-    res.setHeader('Access-Control-Max-Age', "1800")
-    res.setHeader('Access-Control-Allow-Headers', 'X-XSRF-Token-Origin, X-Requested-With, Content, Accept, Accept-Version')
-    res.setHeader('Access-Control-Alllow-Methods', 'GET, POST')
-    next()
-})
-
-
-API.get('/images', (req, res, next)=>{
-    res.json({message : "hey"})
-    res.status(200).json("Erreur de connexion")
-})
-
-API.listen(5000, ()=>{
-    console.log("API démarrée")
 })
 
 module.exports = API
